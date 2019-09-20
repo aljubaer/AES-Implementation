51adfe769063313b9ebc473e86e9e434d048b17c
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2019-09-21 00:10:32 +0600

    first commit

---
+++ engine/python-engine.py
@@ -0,0 +1,7 @@
+import sys
+
+inputText = sys.argv[1]
+inputKey = sys.argv[2]
+
+print("Response from python engine: \n" + "Text: " + inputText + "\n" + "Key: " + inputKey)
+sys.stdout.flush()
---
+++ gui/linkers/python_link.js
@@ -0,0 +1,28 @@
+function getSimpleResponse() {
+    var ps = require("python-shell");
+    var path = require("path");
+
+    var inputMessage = "This is a test input";
+    inputMessage = document.getElementById("plainTextMessage").value;
+    console.log(inputMessage);
+    var key = document.getElementById("plainKey").value;
+
+    var options = {
+        scriptPath: path.join(__dirname, '/../engine/'),
+        args: [inputMessage, key]
+    };
+
+    var res = ps.PythonShell.run('python-engine.py', options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+        document.getElementById("responseText").innerHTML = "The response here ->\n " + results;
+        return results;
+    });
+
+    res.on('message', function (message) {
+        //swal(message);
+        console.log(message);
+        document.getElementById("responseText").value = "The response here -> " + message;
+    });
+}
