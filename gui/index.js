3724a03ed1ed9b5891467329aea7d0195ab08693
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2019-10-12 14:44:46 +0600

    ui updates

--- gui/index.js
+++ gui/index.js
@@ -15,7 +15,7 @@ function createWindow() {
     });
 
     // and load the index.html of the app.
-    win.loadFile("main-frame.html");
+    win.loadFile("index.html");
 
     // Open the DevTools.
     win.webContents.openDevTools();
